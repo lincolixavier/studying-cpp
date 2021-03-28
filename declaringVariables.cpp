ef568c4e4acc570d8355d8524bffbfa3e90cbd52
Lincoli Xavier <lincolixavier@gmail.com>
2021-03-28 01:28:39 -0300

    update progress: variable type progress

---
+++ declaringVariables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main() { 
+
+  int number = 1;
+  char letter = 'L';
+  char letters[15] = 'Lincoli Xavier';
+  float decimalFloat = 2.333;
+  double decimal = 2.5;
+  bool vish = false;
+  string word = "Word";
+  
+  cout << number << "\n\n" << letter << "\n\n" << decimalFloat << "\n\n" << decimal << "\n\n" << vish << "\n\n" << word << "\n\n";
+  
+  return 0;
+}
