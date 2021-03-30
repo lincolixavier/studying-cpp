6d5ae5697b4b581acb9cf9a0e13d35543940d266
Lincoli Xavier <lincolixavier@gmail.com>
2021-03-29 22:00:43 -0300

    multiple variables, constants and define

---
+++ multipleVariablesAndConsts.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+#define pi 3.14159265359
+
+#define myConst cout << "VISH \n";
+
+int main() { 
+  
+  int number = 1, anotherNumber = 11, lastNumber = 11;
+  char alphabet[23];
+
+  myConst
+  cout << pi << "\n";
+  
+  return 0;
+}
