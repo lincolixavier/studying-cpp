0569d57bc13f20f863838440da50798fd3954d59
Lincoli Xavier <lincolixavier@gmail.com>
2021-03-31 21:43:14 -0300

    increment/decrement pos/pre-fixed operators

---
+++ IncrementDecrementPosPre.cpp
@@ -0,0 +1,15 @@
+// Increment and Decrement of Variables, Post and pre-fixed Operators
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n1 = 0, n2 = 10;
+  n1++; // n1 = n1 + 1 | n1 += 1;
+  n2++;
+  n2 *= 2;
+  cout << n1 << "\n\n";
+  cout << n2 << "\n\n";
+
+  return 0;
+}
