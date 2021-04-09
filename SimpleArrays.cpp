14a97601d0825054267b158bedc474d7b78ead48
Lincoli Xavier <lincolixavier@gmail.com>
2021-04-09 19:28:44 -0300

    arrays simples e funcao sizeof

---
+++ SimpleArrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+   int vetor[5]={10,20,30,40,50}; //Cada vetor inteiro "int" tem um valor de 4 bytes, no entanto
+                                  //dependendo do SO cada posição pode ter um tamanho maior que 4 bytes
+   int i;
+
+   for(i=0 ; i<sizeof(vetor)/sizeof(vetor[0]) ; i++){
+      cout << vetor[i] << "\n";
+   }
+
+return 0;
+}
