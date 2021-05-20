3edd4afe3889cc2decc3c85a152dadcbd4d8dcc3
Lincoli Xavier <lincolixavier@gmail.com>
2021-05-19 23:47:06 -0300

    data structures: lists

---
+++ DataStructures/Lists.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <list>
+
+#define LISTA_INT list <int> 
+#define ITERADOR list <int>::iterator 
+
+using namespace std;
+
+void exibir_lista(LISTA_INT lista_inteiro, ITERADOR ponteiro);
+
+int main(void) {
+	
+	LISTA_INT lista;
+	ITERADOR ponteiro_iterador;
+	
+	for(int i = 5; i < 15; i++) {
+ //Preenche a lista
+		lista.push_back(i);
+	}
+	
+	exibir_lista(lista, ponteiro_iterador);
+ //É necessário passar como argumento: uma lista de inteiros e o iterator que deve ser do mesmo tipo da lista 
+	
+	return 0;
+}
+
+void exibir_lista(LISTA_INT lista_inteiro, ITERADOR ponteiro) {
+ //Função para exibir a lista
+	
+	cout << "\n---------------------------------\n";
+	
+	for(ponteiro = lista_inteiro.begin(); ponteiro != lista_inteiro.end(); ponteiro++) {
+		cout << *ponteiro << ' ';
+	}
+
+	cout << "\n---------------------------------\n";
+}
---
+++ DataStructures/Queue.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main() {
+
+    queue <string> Cards;
+
+    Cards.push("Dragão Branco de Olhos Azuis");
+    Cards.push("Mago Negro");
+    Cards.push("Dragão Negro de Olhos Vermelho");
+    Cards.push("Mago do Tempo");
+    Cards.push("Polimerização");
+
+    cout << "Tamanho da fila e de " << Cards.size() << " Cards \n  -- Merendeira esta disbuindo os lanches \n";
+    cout << "_________________________________ \n";
+    int tam = Cards.size();
+
+    while(!Cards.empty()){
+            cout << "O " << Cards.front() << " Pegar o lanche e sair da fila" << endl;
+            Cards.pop();
+    }
+    cout << "_________________________________ \n A fila esta vazia.";
+return 0;
+}
